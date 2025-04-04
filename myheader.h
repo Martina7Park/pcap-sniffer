191b044fb965f7aefa7bd0b3a5c4038b110275a4
Martina7Park <john1515700@gmail.com>
2025-04-04 17:14:41 +0900

    myheader.h

--- myheader.h
+++ myheader.h
@@ -6,11 +6,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <netinet/ip.h>      // IP 헤더 구조체
-#include <netinet/tcp.h>     // TCP 헤더 구조체
-#include <net/ethernet.h>    // Ethernet 헤더 구조체
-#include <arpa/inet.h>       // IP 출력용
+#include <netinet/ip.h>      
+#include <netinet/tcp.h>    
+#include <net/ethernet.h>   
+#include <arpa/inet.h>     
 
-// 필요시 사용자 정의 구조체 추가 가능
 
 #endif
