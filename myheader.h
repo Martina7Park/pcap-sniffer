39f44dcdea5a6edcdd2ff22c5230844209dfab38
Martina7Park <john1515700@gmail.com>
2025-04-04 17:14:11 +0900

    Create myheader.h

---
+++ myheader.h
@@ -0,0 +1,16 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+#include <pcap.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <netinet/ip.h>      // IP 헤더 구조체
+#include <netinet/tcp.h>     // TCP 헤더 구조체
+#include <net/ethernet.h>    // Ethernet 헤더 구조체
+#include <arpa/inet.h>       // IP 출력용
+
+// 필요시 사용자 정의 구조체 추가 가능
+
+#endif
